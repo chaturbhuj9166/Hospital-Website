7c34335464d3cb2c950bf0d8ebab3967c1fe9fd4
Prem <joshichaturbhuj2@gmail.com>
2026-03-31 16:22:08 -0400

    fix cors error

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,13 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
     credentials: true,
   })
 );
